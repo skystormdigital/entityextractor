1eb8017f3951e7341b2baa0183690db0b62eb856
skystormdigital <skystormdigital@gmail.com>
2025-06-29 13:41:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ if run_button:
                 # Locate Wikidata link inside the LOD block if present.
                 wikidata_url = None
                 for lod_match in ann.get("lod", []):
-                    if (wikidata_id := lod_match.get("wikidata")):
+                    wikidata_id = lod_match.get("wikidata")
+                    if wikidata_id:
                         wikidata_url = f"https://www.wikidata.org/wiki/{wikidata_id}"
                         break
 
